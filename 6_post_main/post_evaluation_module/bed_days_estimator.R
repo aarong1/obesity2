eb75bc75123d57da86c17d41fe83ae3a1c7b37a8
Aaron Gorman <gormanaa@tcd.ie>
2026-07-29 23:29:16 +0100

    fix:update

--- 6_post_main/post_evaluation_module/bed_days_estimator.R
+++ 6_post_main/post_evaluation_module/bed_days_estimator.R
@@ -58,7 +58,7 @@ bed_days_estimator <- function( past_populations,stats2223_agg) {
 #     e_line(bed_days)
 }
 
-if(!interactive()){
+if(interactive()){
   print('yes')
 # unit_test <- function(){
 bed_days_estimator(total_pop,stats2223_agg ) |> 
