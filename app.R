103d7f20955c0839e345f13499ec6c3d30436d82
Aaron Gorman <gormanaa@tcd.ie>
2026-07-30 00:02:01 +0100

    fix:update

--- app.R
+++ app.R
@@ -15,6 +15,9 @@ library(sf)
 
 
 print(system('ls ~'))
+print(system('ls/opt/R/4.5.1/lib/R/library'))
+print(system('ls/opt/R/4.5.1/lib/R'))
+      
 print(resourcePaths())
 
 dot <- function(col){
